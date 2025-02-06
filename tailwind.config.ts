1dfe72a711757fc104047e130ee134c803dbda79
Ryan McGuire <ryan.mcguire.va@gmail.com>
2025-02-06 12:47:51 -0500

    font style setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				red: 'hsl(0, 97%, 63%)',
+				bluePrimary: 'hsl(223, 30%, 9%)',
+				blueSecondary: 'hsl(223, 23%, 46%)',
+				blueTertiary: 'hsl(223, 36%, 14%)',
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
