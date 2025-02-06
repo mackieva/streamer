9384cbf33a5bf995c8b631c08dee8cf7313dc949
Ryan McGuire <ryan.mcguire.va@gmail.com>
2025-02-06 13:20:06 -0500

    sign up form

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,19 +1,19 @@
-import SignInForm from './sign-in-form';
+import SignUpForm from './sign-up-form';
 import Image from 'next/image';
 import Link from 'next/link';
 import logo from '@/public/images/logo.svg';
 
-const SignInPage = () => {
+const SignUpPage = () => {
 	return (
 		<section className='flex flex-col justify-start items-center'>
 			<Image src={logo} alt='Streaming Logo' className='mt-20 mb-20' />
 			<main className='w-[400px] p-8 rounded-xl bg-blueTertiary placeholder:text-blueSecondary'>
-				<h2 className='heading-l mb-10'>Login</h2>
-				<SignInForm />
+				<h2 className='heading-l mb-10'>Sign Up</h2>
+				<SignUpForm />
 				<p className='text-center'>
-					Don't have an account?{' '}
-					<Link href='/sign-up' className='text-red'>
-						Sign Up
+					Already have an account?{' '}
+					<Link href='/sign-in' className='text-red'>
+						Login
 					</Link>
 				</p>
 			</main>
@@ -21,4 +21,4 @@ const SignInPage = () => {
 	);
 };
 
-export default SignInPage;
+export default SignUpPage;
---
+++ app/sign-up/sign-up-form.tsx
@@ -0,0 +1,35 @@
+const SignUpForm = () => {
+	return (
+		<form className='flex flex-col mb-6'>
+			<input
+				type='text'
+				placeholder='Email Address'
+				id='email'
+				name='email'
+				className='w-full border-b-[1px] border-b-blueSecondary pl-4 pb-4 mb-6'
+			/>
+			<input
+				type='password'
+				name='password'
+				id='password'
+				placeholder='Password'
+				className='w-full border-b-[1px] border-b-blueSecondary pl-4 pb-4 mb-6'
+			/>
+			<input
+				type='password'
+				name='confirm-pasword'
+				id='confirm-pasword'
+				placeholder='Repeat password'
+				className='w-full border-b-[1px] border-b-blueSecondary pl-4 pb-4 mb-10'
+			/>
+			<button
+				type='submit'
+				className='bg-red rounded-md text-white body-m py-3 hover:bg-white hover:text-blueTertiary transition-all'
+			>
+				Create an account
+			</button>
+		</form>
+	);
+};
+
+export default SignUpForm;
