300e99ab5baa1ddbb5c37396ef8aec4efe53fa70
Ryan McGuire <ryan.mcguire.va@gmail.com>
2025-02-10 10:54:32 -0500

    user signout button

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,7 @@
-import { auth } from '@/auth';
-import { redirect } from 'next/navigation';
 import { getAllMedia } from '@/lib/actions/media.actions';
 import MediaGrid from '@/components/media/media-grid';
 import MediaTrending from '@/components/media/media-trending';
+import SignOutButton from '@/components/test/signoutbtn';
 export default async function Home() {
 	const media = await getAllMedia();
 	const trending = media.filter((item) => item.isTrending === true);
@@ -11,6 +10,7 @@ export default async function Home() {
 	const sortedMedia = movies.concat(television);
 	return (
 		<section>
+			<SignOutButton />
 			<h2 className='mb-8 heading-l'>Trending</h2>
 			<MediaTrending media={trending} />
 			<h2 className='mt-8 mb-8 heading-l'>Recommended for you</h2>
