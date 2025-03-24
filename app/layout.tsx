a72920acf2ae3e724fb3bf53888ddc1b611e583e
Ryan McGuire <ryan.mcguire.va@gmail.com>
2025-03-24 10:24:33 -0400

    vercel analytics setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Outfit } from 'next/font/google';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 const outfit = Outfit({
 	variable: '--font-outfit',
@@ -23,7 +24,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body className={`${outfit.variable} $antialiased`}>{children}</body>
+			<body className={`${outfit.variable} $antialiased`}>
+				{children}
+				<Analytics />
+			</body>
 		</html>
 	);
 }
